d64e0750d5f862d598e04eafa69f0328944133c7
dhwanitsshah <dhwanit.s.shah@gmail.com>
2014-12-10 02:48:03 +0530

    Generator code and sample generated files

---
+++ generator/src/root/blocking/blockingImpl.py
@@ -0,0 +1,184 @@
+
+from root.models.name import Name
+from root.models.blockingPatient import BlockingPatient
+
+class Blocking:
+    
+    def getNames(self):
+        import pymysql
+        conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='root', db='openmrs')  # else connect to default mysqldb
+        cur = conn.cursor()
+        query = "select pn.uuid,pn.given_name,pn.middle_name,pn.family_name,DATE_FORMAT(pe.birthdate,'%y'),DATE_FORMAT(pe.birthdate,'%m'),DATE_FORMAT(pe.birthdate,'%d') from person pe,person_name pn where pe.uuid=pn.uuid"
+        query += " limit 100"
+        cur.execute(query)
+        blockingPatients = []
+                
+        for row in cur:
+            uuid = row[0]
+            givenName = row[1]
+            middleName = row[2]
+            familyName = row[3]
+            yob = row[4]
+            mob = row[5]
+            dayob = row[6]
+            name = Name(givenName, middleName, familyName)
+            p = BlockingPatient()
+            p.setName(name)
+            p.setUuid(uuid)
+            p.setYob(yob)
+            p.setMob(mob)
+            p.setDayob(dayob)
+            blockingPatients.append(p)
+            
+        cur.close()
+        conn.close()
+        return blockingPatients
+    
+    
+    #[ B3, C3, Y2 ] First 3 characters of Names B and Name C and the last 2 digits of the year of birth (JUDWAW80)
+    def SortedBlockingScheme1(self,blockingPatients):
+        blockingColumn = {}
+        for p in blockingPatients:
+            b = p.sortedNameList[1]
+            c = p.sortedNameList[2]
+            b = b[0:3]
+            c = c[0:3]
+            s = b + c + p.yob
+            blockingColumn.update({str(p.uuid) : s.upper()})
+        return blockingColumn
+    
+    #[ B3, Blk-DB , Blk-MB ] First 3 characters of Name B, date and month of birth (JUD2703)
+    def SortedBlockingScheme2(self,blockingPatients):
+        blockingColumn = {}
+        for p in blockingPatients:
+            b = p.sortedNameList[1]
+            b = b[0:3]
+            s = b + p.dayob + p.mob
+            blockingColumn.update({str(p.uuid) : s.upper()})
+        return blockingColumn
+                
+    #[ C3, Blk-DB, Blk-MB ] First 3 characters of Name C, date and month of birth (WAW2703)
+    def SortedBlockingScheme3(self,blockingPatients):
+        blockingColumn = {}
+        for p in blockingPatients:
+            c = p.sortedNameList[2]
+            c = c[0:3]
+            s = c + p.dayob + p.mob
+            blockingColumn.update({str(p.uuid) : s.upper()})
+        return blockingColumn
+        
+    #[ B1, YB, Blk-DB, Blk-MB ] First character of Name B and date of birth (J032780)
+    def SortedBlockingScheme4(self,blockingPatients):
+        blockingColumn = {}
+        for p in blockingPatients:
+            b = p.sortedNameList[1]
+            b = b[0:1]
+            s = b + p.mob + p.dayob + p.yob
+            blockingColumn.update({str(p.uuid) : s.upper()})
+        return blockingColumn
+    
+    #[ C1, YB, Blk-DB, Blk-MB ] First character of Name C and date of birth (W032780)
+    def SortedBlockingScheme5(self,blockingPatients):
+        blockingColumn = {}
+        for p in blockingPatients:
+            c = p.sortedNameList[2]
+            c = c[0:1]
+            s = c + p.mob + p.dayob + p.yob
+            blockingColumn.update({str(p.uuid) : s.upper()})
+        return blockingColumn
+    
+    #Combination Schemes for unsorted names list 
+    
+    #[ B3, C3, Y2 ] First 3 characters of Names B and Name C and the last 2 digits of the year of birth (JUDWAW80)
+    def UnSortedBlockingScheme1(self,blockingPatients):
+        blockingColumn = {}
+        for p in blockingPatients:
+            b = p.UnSortedNameList[1]
+            c = p.UnSortedNameList[2]
+            b = b[0:3]
+            c = c[0:3]
+            s = b + c + p.yob
+            blockingColumn.update({str(p.uuid) : s.upper()})
+        return blockingColumn
+    
+    #[ B3, Blk-DB , Blk-MB ] First 3 characters of Name B, date and month of birth (JUD2703)
+    def UnSortedBlockingScheme2(self,blockingPatients):
+        blockingColumn = {}
+        for p in blockingPatients:
+            b = p.UnSortedNameList[1]
+            b = b[0:3]
+            s = b + p.dayob + p.mob
+            blockingColumn.update({str(p.uuid) : s.upper()})
+        return blockingColumn
+                
+    #[ C3, Blk-DB, Blk-MB ] First 3 characters of Name C, date and month of birth (WAW2703)
+    def UnSortedBlockingScheme3(self,blockingPatients):
+        blockingColumn = {}
+        for p in blockingPatients:
+            c = p.UnSortedNameList[2]
+            c = c[0:3]
+            s = c + p.dayob + p.mob
+            blockingColumn.update({str(p.uuid) : s.upper()})
+        return blockingColumn
+        
+    #[ B1, YB, Blk-DB, Blk-MB ] First character of Name B and date of birth (J032780)
+    def UnSortedBlockingScheme4(self,blockingPatients):
+        blockingColumn = {}
+        for p in blockingPatients:
+            b = p.UnSortedNameList[1]
+            b = b[0:1]
+            s = b + p.mob + p.dayob + p.yob
+            blockingColumn.update({str(p.uuid) : s.upper()})
+        return blockingColumn
+    
+    #[ C1, YB, Blk-DB, Blk-MB ] First character of Name C and date of birth (W032780)
+    def UnSortedBlockingScheme5(self,blockingPatients):
+        blockingColumn = {}
+        for p in blockingPatients:
+            c = p.UnSortedNameList[2]
+            c = c[0:1]
+            s = c + p.mob + p.dayob + p.yob
+            blockingColumn.update({str(p.uuid) : s.upper()})
+        return blockingColumn
+    
+    
+    def insertBlockingSchemeTable(self,schemeName,combination,blockingColumn):
+        import pymysql
+        conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='root', db='openmrs')  # else connect to default mysqldb
+        cur = conn.cursor()
+        
+        for value in blockingColumn:
+            query = "REPLACE INTO blockingscheme (uuid,"+schemeName+",combination) values('"+value+"','"+blockingColumn[value]+"','"+combination+"')"
+            cur.execute(query)
+        
+        conn.commit()    
+        cur.close()
+        conn.close()
+        return
+    
+    
+    def updateBlockingSchemeTable(self,schemeName,combination, blockingColumn):
+        import pymysql
+        conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='root', db='openmrs')  # else connect to default mysqldb
+        cur = conn.cursor()
+        
+        for value in blockingColumn:
+            query = "UPDATE blockingscheme set "+schemeName+" = '"+blockingColumn[value]+"' where uuid='"+value+"'"
+            cur.execute(query)
+        
+        conn.commit()    
+        cur.close()
+        conn.close()
+        return
+    
+    def truncateBlockingSchemeTable(self):
+        import pymysql
+        conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='root', db='openmrs')  # else connect to default mysqldb
+        cur = conn.cursor()
+        query = "TRUNCATE table blockingscheme"
+        cur.execute(query)
+        conn.commit()    
+        cur.close()
+        conn.close()
+        return
+    
---
+++ generator/src/root/models/name.py
@@ -0,0 +1,28 @@
+
+class Name:
+    
+    def __init__(self,givenName,middleName,familyName):
+        self.givenName = givenName
+        self.middleName = middleName
+        self.familyName = familyName
+        
+    def __str__(self):
+        return(self.givenName + "," + self.middleName + "," +self.familyName)
+    
+    def __dir__(self):
+        return(["givenName","middleName","familyName"])
+    
+    def getSortedNamesAsList(self):
+        lst = []
+        lst.append(self.givenName)
+        lst.append(self.middleName)
+        lst.append(self.familyName)
+        lst.sort()
+        return lst
+    
+    def getUnSortedNamesAsList(self):
+        lst = []
+        lst.append(self.givenName)
+        lst.append(self.middleName)
+        lst.append(self.familyName)
+        return lst
